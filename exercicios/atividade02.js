a805bdd6fcd6f25157ddfd0cfc02112f52ad7fe6
César Barbosa <edinaldocesar14@gmail.com>
2023-02-22 15:45:34 -0300

    atualizando a atividade02

--- exercicios/atividade02.js
+++ exercicios/atividade02.js
@@ -1,4 +1,5 @@
 let salario = 1300;
 let porcentagem = salario*25/100;
 let salarioFinal = salario + porcentagem;
-console.log(salarioFinal);
+console.log("O salário inicial era de:", salario);
+console.log("O salário com 25% de aumento ficará:", salarioFinal);
