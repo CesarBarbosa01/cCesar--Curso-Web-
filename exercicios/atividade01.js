541146c590ff8a7c5f52e6b947e887d741eb6d69
César Barbosa <93480237+CesarBarbosa01@users.noreply.github.com>
2023-02-24 09:01:19 -0300

    adicionando detalhes

--- exercicios/atividade01.js
+++ exercicios/atividade01.js
@@ -1,12 +1,13 @@
 let a = 10;
 let b = 20;
+console.log("Valor de A:", a);
+console.log("Valor de B", b);
 
 let c = a;
 a = b;
 b = c;
-
-console.log(a);
-console.log(b);
+console.log("Valor atualizado de A:",a);
+console.log("Valor atualizado de B:",b);
 
 
 
